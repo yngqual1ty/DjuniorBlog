c37e1e9171d7efa5143e998ad335cb79e3d2cbf5
yngqual1ty <b0ch0k@yandex.ru>
2024-11-02 23:57:59 +0700

    first checkout for new branch

--- main/models.py
+++ main/models.py
@@ -7,6 +7,6 @@ class Publication(models.Model):
     date = models.DateTimeField(datetime.now)
     shortDescription = models.CharField(max_length=10000, verbose_name='Короткое описание')
     content = models.TextField(verbose_name='Контент')
-    clickCount = models.IntegerField(verbose_name='Количество переходов', default=0)
+    clickCount = models.IntegerField(verbose_name='Количество переходов', default=0),
     def __str__(self):
         return self.title
