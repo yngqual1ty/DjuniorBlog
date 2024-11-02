5e066baa515071e80962e2072af6fbc301c64e61
yngqual1ty <b0ch0k@yandex.ru>
2024-11-03 02:21:23 +0700

    add CreateBlog, EditBlog pages. Add delete,edit,crate functions for blog. little some changes in app.

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('about/', views.about, name='about'),
+    path('createBlog/', views.createBlog, name='createBlog'),
     path('blog/<int:publicationId>/', views.blog, name='blog'),
+    path('deleteBlog/<int:publicationId>/', views.deleteBlog, name='deleteBlog'),
+    path('editBlog/<int:publicationId>/', views.editBlog, name='editBlog'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
 from .models import Publication
+from .forms import CreateBlogForm
+
 def homepage(request):
     publications = Publication.objects.all().order_by('-date')
     if Publication.objects.all().order_by('-clickCount').count() < 3:
@@ -8,8 +12,10 @@ def homepage(request):
         popularPublications = Publication.objects.all().order_by('-clickCount')[:3]
     return render(request, 'main/homepage.html', {'publications':publications, 'firstThreePublications':popularPublications})
 
+
 def about(request):
     return render(request, 'main/about.html')
+
 
 def blog(request, publicationId):
     publicationData = Publication.objects.get(id=publicationId)
@@ -17,3 +23,36 @@ def blog(request, publicationId):
     publicationData.save()
 
     return render(request, 'main/blog.html', {'publicationData':publicationData})
+
+
+def createBlog(request):
+    error = ''
+    if request.method == 'POST':
+        form = CreateBlogForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('homepage')
+        else:
+            error = 'Неверно заполнена форма'
+
+    form = CreateBlogForm()
+    context = {'form': form, 'error': error}
+    return render(request, 'main/createBlog.html', context)
+
+
+def deleteBlog(request, publicationId):
+    Publication.objects.get(id=publicationId).delete()
+    return redirect('homepage')
+
+
+def editBlog(request, publicationId):
+    error = ''
+    if request.method == 'POST':
+        form = CreateBlogForm(request.POST, instance=Publication.objects.get(id=publicationId))
+        if form.is_valid():
+            form.save()
+            return redirect('homepage')
+
+    form = CreateBlogForm(instance=Publication.objects.get(id=publicationId))
+    context = {'form': form, 'error': error}
+    return render(request, 'main/editBlog.html', context)
